8adac74181309ad482a85e7927f2fa687f6a5b77
Javier Contreras <jcontreras@binarybag.com>
2025-06-13 00:12:20 -0400

    20 mb para fotos

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const corsOptions = {
   };
 
 app.use(cors(corsOptions));
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: true, limit: '20mb' }));
 
 // Servir imágenes
 const __filename = fileURLToPath(import.meta.url);
